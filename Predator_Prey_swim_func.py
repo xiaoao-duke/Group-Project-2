cc69475d1eec16d9ec6772de1f8541f31d59c886
xw89 <xinyu.wu@duke.edu>
2016-04-03 00:19:44 -0400

    Update Predator_Prey_swim_func.py
    Periodic boundary added, still possible to have shark and fish conflict ( eg: 2 fish swim to the same place) at boundaries.

--- Predator_Prey_swim_func.py
+++ Predator_Prey_swim_func.py
@@ -4,15 +4,16 @@ from pylab import*
 Fish_number = []
 Shark_number = []
 size = 100 # grid size
-breed_age = 15
+breed_age_fish = 15
+breed_age_shark=50
+starve_time=10
 
 Fish = -1*np.ones((size+1, size+1), dtype=np.int)
 Shark = -1*np.ones((size+1, size+1), dtype=np.int)
-Fishmove = np.ones((size+1, size+1), dtype=np.int)
-Sharkmove = np.ones((size+1, size+1), dtype=np.int)
-Sharkstarve = np.ones((size+1, size+1), dtype=np.int)
-Fish[5,5] = 13
-print Fish
+Fishmove = -1*np.ones((size+1, size+1), dtype=np.int)
+Sharkmove = -1*np.ones((size+1, size+1), dtype=np.int)
+Sharkstarve = np.zeros((size+1, size+1), dtype=np.int)
+
 # initial condition for Fish
 for j in range(100):
     for i in range(49):
@@ -23,9 +24,8 @@ for j in range(25):
         Shark[4*j,4*i+mod(j,4)]=0
 
 def Fish_swim_breed(yu,breed_age):
-    fishmove=zeros([size+2,size+2])
-    occupied=zeros([size+2,size+2])
-    fish=zeros([size+2,size+2])
+    fishmove=-1*ones([size+2,size+2])
+    fish=-1*ones([size+2,size+2])
     fish[1:size+1,1:size+2]=yu   
     fish[0,1:size+1]=yu[size-1]  # match top with bottom to achieve periodic boundary
     fish[size+1,1:size+2]=yu[0]
@@ -36,88 +36,120 @@ def Fish_swim_breed(yu,breed_age):
         for j in range(1,size+1)
             if fish[i,j] != -1: # if there is a fish in (i,j)
                 choice = np.zeros(4)
-                if (fish[i-1,j] == -1)&(occupied[i-1,j]==0):
+                if (fish[i-1,j] == -1)&(fishmove[i-1,j]==0):
                     choice[0] = 1 # record index if left position is vacant and will be vacant later
-                if (fish[i+1,j] == -1)&(occupied[i+1,j]==0): 
+                if (fish[i+1,j] == -1)&(fishmove[i+1,j]==0): 
                     choice[1] = 2 # record index if right position is vacant
-                if (fish[i, j+1] == -1)&(occupied[i,j-1]==0):
+                if (fish[i, j+1] == -1)&(fishmove[i,j-1]==0):
                     choice[2] = 3 # record index if up position is vacant
-                if (fish[i, j-1] == -1)&(occupied[i,j+1]==0):
+                if (fish[i, j-1] == -1)&(fishmove[i,j+1]==0):
                     choice[3] = 4 # record index if down position is vacant
                 Choice = filter(None, choice) # only vacant positions are reserved
                 temp = [(i-1,j), (i+1,j), (i,j+1), (i,j-1)] # temperary storage for index
                 if np.size(Choice)>0:
                     index = np.random.choice(Choice)-1 # choose one vacant position randomly
                     if fish[i,j]<breed_age:
-                        fishmove[temp[index]] = fishmove[temp[index]]+fish(i,j)+1  # a chosen position is replaced by fish (i,j) and age is increased
-                        fishmove[i,j]=fishmove[i,j]-fish[i,j]-1 # action of erasing current fish
-                        occupied[temp[index]]=1
+                        fishmove[temp[index]]=fish[i,j]+1  # a chosen position is replaced by fish (i,j) and age is increased
                     else:  # if fish reach breed age
-                        fishmove[temp[index]]=1
-                        fishmove[i,j]=-fish[i,j]
-                        occupied[temp[index]]=1
+                        fishmove[temp[index]]=0
+                        fishmove[i,j]=0
                 else:
-                    fishmove[i,j]=fishmove[i,j]+1 # if no place to go then stay there and age
+                    fishmove[i,j]=fish[i,j]+1 # if no place to go then stay there and age
     fishmove[1,1:size+1]=fishmove[1,1:size+1]+fishmove[size+1,1:size+1] # top boundary include from bottom to top(periodic boundary)
     fishmove[size,1:size+1]=fishmove[size,1:size+1]+fishmove[0,1:size+1]
     fishmove[1:size+1,1]=fishmove[1:size+1,1]+fishmove[1:size+1,size+1]
     fishmove[1:size+1,size]=fishmove[1:size+1,size]+fishmove[1:size+1,0]
     return fishmove[1:size+1,1:size+1]
-def Shark_hunt_breed(shayu,shayustarve,fish,breed_age)
-    sharkmove=zeros([size+2,size+2])
-    occupied=zeros([size+2,size+2])
+    
+# shark hunt and breed
+def Shark_hunt_breed(shayu,shayustarve,fish,stave_time,breed_age)
+    sharkmove=-1*ones([size+2,size+2])
     sharkstarve=zeros([size+2,size+2]) # record shark starving time
-    shark=zeros([size+2,size+2])
+    shark=-1*ones([size+2,size+2])
     shark[1:size+1,1:size+2]=shayu   
     shark[0,1:size+1]=shayu[size-1]  # match top with bottom to achieve periodic boundary
     shark[size+1,1:size+1]=shayu[0]
     shark[1:size+1,0]=shayu[:,size-1]
     shark[1:size+1,size+1]=shayu[:,0]
-    sharkeat[1:size+1,1:size+1]=shayustarve
 # main part(exclude boundaries)
     for i in range(1,size+1)
         for j in range(1,size+1)
             if shark[i,j] != -1: # if there is a shark in (i,j)
-                choice = np.zeros(4)
-                if (fish[i-1,j] != -1)&(occupied[i-1,j]==0):
-                    choice[0] = 1 # record index if there is a fish at (i-1,j) and no shark is coming
-                if (fish[i+1,j] != -1)&(occupied[i+1,j]==0): 
-                    choice[1] = 2 
-                if (fish[i, j+1] != -1)&(occupied[i,j-1]==0):
-                    choice[2] = 3
-                if (fish[i, j-1] != -1)&(occupied[i,j+1]==0):
-                    choice[3] = 4
-                Choice = filter(None, choice) # only fish and no shark coming spot are reserved
-                temp = [(i-1,j), (i+1,j), (i,j+1), (i,j-1)] # temperary storage for index
-                if np.size(Choice)>0:
-                    index = np.random.choice(Choice)-1 # choose one position randomly
+                planA = np.zeros(4) # record choices with fish and no shark at a spot
+                planB=np.zeros(4) # record choices without fish but no shark
+                if (shark[i-1,j]==-1)&(sharkmove[i-1,j]==-1):
+                    if fish[i-1,j]!=1:
+                        planA[0]=1
+                    else:
+                        planB[0]=1
+                if (shark[i+1,j]==-1)&(sharkmove[i+1,j]==-1): 
+                    if fish[i+1,j]!=-1:
+                        planA[1]=1
+                    else:
+                        planB[1]=1
+                if (shark[i,j-1]==-1)&(sharkmove[i,j-1]==-1):
+                    if fish[i,j-1]!=-1
+                        planA[2]=1
+                    else:
+                        planB[2]=1
+                if (shark[i,j+1]==-1)&(sharkmove[i,j+1]==-1):
+                    if fish[i,j+1]!=-1
+                        planA[3]=1
+                    else:
+                        planB[3]=1
+                PlanA = filter(None,planA) # only fish and no shark coming spot are reserved
+                PlanB=filter(None,planB) # no fish around spot without shark are reserved
+                temp = [(i-1,j), (i+1,j), (i,j-1), (i,j+1)] # temperary storage for index
+                if np.size(planA)>0:
+                    index = np.random.choice(planA)-1 # choose one position randomly
                     if shark[i,j]<breed_age:
-                        sharkmove[temp[index]] = sharkhmove[temp[index]]+shark[i,j]+1  # a chosen position is replaced by fish (i,j) and age is increased
-                        sharkmove[i,j]=sharkmove[i,j]-shark[i,j]-1 # action of erasing current fish
-                        occupied[temp[index]]=1
-                        sharkstarve[i,j]=0
+                        sharkmove[temp[index]] =shark[i,j]+1  # a chosen position is replaced by fish (i,j) and age is increased
+                        sharkstarve[temp[index]]=0
                     else:  # if shark reach breed age
-                        sharkhmove[temp[index]]=1
-                        sharkmove[i,j]=-shark[i,j]
-                        occupied[temp[index]]=1
-            else:
-                fishmove[i,j]=fishmove[i,j]+1 # if no place to go then stay there and age
-    fishmove[1,1:size+2]=fishmove[1,1:size+2]+fishmove[size+1,1:size+2] # top boundary include from bottom to top(periodic boundary)
-    fishmove[size,1:size+2]=fishmove[size,1:size+2]+fishmove[0,1:size+2]
-    fishmove[1:size+2,1]=fishmove[1:size+2,1]+fishmove[1:size+2,size+1]
-    fishmove[1:size+2,size]=fishmove[1:size+2,size]+fishmove[1:size+2,0]
-    return fishmove[1:size+2,1:size+2]
+                        sharkhmove[temp[index]]=shark[i,j]+1
+                        sharkmove[i,j]=0
+                        sharkstarve[temp[index]]=0
+                        sharkstarve[i,j]=0
+                elif np.size(planB)>0:
+                    index=np.random.choice(planB)-1
+                    if shayustarve[index]+1<starve_time:
+                        if shark[i,j]<breed_age:
+                            sharkmove[temp[index]]=shark[i,j]+1
+                            sharkstarve[temp[index]]=shayustarve[i,j]+1
+                        else:
+                            sharkmove[temp[index]]=0
+                            sharkmove[i,j]=0
+                            sharkstarve[temp[index]]=shayustarve[i,j]+1
+                            sharkstarve[i,j]=shayustarve[i,j]+1
+                else: # shark is surrounded by shark, shark do not move
+                    if shayustarve[i,j]+1<starve_time:
+                        sharkmove[i,j]=shark[i,j]+1
+                        sharkstarve[i,j]=shayustarve[i,j]+1
+# match boundaries
+    sharkmove[1,1:size+1]=sharkmove[1,1:size+1]+sharkmove[size+1,1:size+1]
+    sharkmove[size,1:size+1]=sharkmove[size,1:size+1]+sharkmove[0,1:size+1]
+    sharkmove[1:size+1,1]=sharkmove[1:size+1,1]+sharkmove[1:size+1,size+1]
+    sharkmove[1:size+1,size]=sharkmove[1:size+1,size]+sharkmove[1:size+1,0]
+    sharkstarve[1,1:size+1]=sharkstarve[1,1:size+1]+sharkstarve[size+1,1:size+1]
+    sharkstarve[size,1:size+1]=sharkstarve[size,1:size+1]+sharkstarve[0,1:size+1]
+    sharkstarve[1:size+1,1]=sharkstarve[1:size+1,1]+sharkstarve[1:size+1,size+1]
+    sharkstarve[1:size+1,size]=sharkstarve[1:size+1,size]+sharkstarve[1:size+1,0]
+    return (sharkmove[1:size+1,1:size+1],sharkstarve[1:size+1,1:size+1])
+
+# when shark encounter a fish, erase the fish
+def fisheaten(fish,shark)
+    for i in range(100):
+        for j in range(100):
+            if shark[i,j]!=-1:
+                fish[i,j]=-1
+    return fish
+
+# order for one time step: 1.fish swim and breed 2. shark hunt breed 3. shark eat fish
+Fish=Fish_swim_breed(Fish,breed_age_fish)
+Shark,SharkStarve=Shark_hunt_breed(Shark,SharkStarve,Fish,starve_time,breed_age_shark)
+                
 
 
-
-
-
-
-
-Fishmove=Fish_swim_breed
-Fish=Fish+Fishmove
-print Fish     
-            
             
             
             
